2abff9eef1fcb419238b8384ef3eda6bb8a1a89b
olgaizmana1 <olga.izmana@outlook.com>
2022-04-26 17:17:28 +0300

    ok

--- CLesson_3/Homework.cs
+++ CLesson_3/Homework.cs
@@ -1,10 +1,18 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading;
 
-namespace C_Sharp_Lesson_3_Homework
+namespace Homework
 {
     public class Homework
     {
-        public void GetCentralElementFromMatrix(int[,] matrixOfIntegers)
+        int[,] matrix1 = new int[3, 3] { { 1, 3, 5 }, { -1, 100, 11 }, { 2, 15, 44 } };
+        int[,] matrix2 = new int[3, 4] { { 1, 6, 21, 8 }, { 5, -4, 5, 7 }, { 77, 5, 0, 74 } };
+
+        public void GetCentralElementFromMatrix1(int[,] matrixOfIntegers)
         {
             /*print to console the central element from matrixOfIntegers, if not exist print: "This matrix doesn't have a central element"
              * |   input           | result             |
@@ -25,65 +33,74 @@ namespace C_Sharp_Lesson_3_Homework
              */
             //your code here
 
+            int midlength0m1 = matrix1.GetLength(0) / 2;
+            int midlength1m1 = matrix1.GetLength(1) / 2;
+
+            if (matrix1.GetLength(0) % 2 != 0 || matrix1.GetLength(1) % 2 != 0)
+            {
+                Console.WriteLine("The central element is " + matrix1[midlength0m1, midlength1m1]);
+            }
+            else
+            {
+                Console.WriteLine("This matrix doesn't have a central element");
+            }
 
         }
-        public void GetSummOfDiagonalsElements(int[,] matrixOfIntegers)
+        public void GetCentralElementFromMatrix2(int[,] matrixOfIntegers)
         {
-            /*print to console the central element from matrixOfIntegers, if not exist print: "This matrix doesn't have a central element"
-             * |   input           | result              |
-             * |-------------------|---------------------|
-             * | {                 |                     |
-             * |  { 1,   3,  5},   | First diagonal: 145 |
-             * |  {-1, 100, 11},   | Second diagonal: 107|
-             * |  { 2,  15, 44}    |                     |
-             * |  }                |                     |
-             * |-----------------------------------------|
-             * |{                  |                     |
-             * | { 1,  6, 21,  8 },| This matrix doesn't |
-             * | { 5, -4,  5,  7 },| have a diagonals    |
-             * | {77,  5,  0, 74 } |                     |
-             * | }                 |                     |
-             * ------------------------------------------
-             *    
-             */
-            //your code here
+            int midlength0m2 = matrix2.GetLength(0) / 2;
+            int midlength1m2 = matrix2.GetLength(1) / 2;
 
-        }
-        public void StarPrinter(int triangleHight)
-        {
-            /* Write a programm that will print a triagle of stars  with hight = triangleHight
-             *  Example: triangleHight = 3;
-             *  Result:   *
-             *           * *
-             *          * * * 
-             */
-            //your code here
-        }
-        public void SortList(IList<int> listOfNumbers)
-        {
-            //Print to console elements of  listOfNumbers in ascending order
-            //your code here
-        }
-        public static void Main(String[] args)
-        {
-            Homework homework = new Homework();
-            IList<int> list = new List<int>() { -5, 8, -7, 0, 44, 121, -7 };
-            int[,] matrix = new int[3, 3] {
-                { 1,   3,  5},
-                { 2, 3, 5},
-                {100, 56 , -54} };
-            int[,] matrix2 = new int[3, 4] {
-                { 1,   3,  5,   6},
-                { 2,   3,  5,  78},
-                {100, 56 , -54, 6} };
-
-            homework.GetCentralElementFromMatrix(matrix);
-            homework.GetCentralElementFromMatrix(matrix2);
-            homework.GetSummOfDiagonalsElements(matrix);
-            homework.GetSummOfDiagonalsElements(matrix2);
-            homework.StarPrinter(5);
-            homework.SortList(list);
+            if (matrix2.GetLength(0) % 2 == 0 || matrix2.GetLength(1) % 2 == 0)
+            {
+                Console.WriteLine("The central element is " + matrix2[midlength0m2, midlength1m2]);
+            }
+            else
+            {
+                Console.WriteLine("This matrix doesn't have a central element");
+            }
+        
         }
 
-    }
+                public void GetSummOfDiagonalsElements(int[,] matrixOfIntegers)
+                {
+                    /*print to console the central element from matrixOfIntegers, if not exist print: "This matrix doesn't have a central element"
+                     * |   input           | result              |
+                     * |-------------------|---------------------|
+                     * | {                 |                     |
+                     * |  { 1,   3,  5},   | First diagonal: 145 |
+                     * |  {-1, 100, 11},   | Second diagonal: 107|
+                     * |  { 2,  15, 44}    |                     |
+                     * |  }                |                     |
+                     * |-----------------------------------------|
+                     * |{                  |                     |
+                     * | { 1,  6, 21,  8 },| This matrix doesn't |
+                     * | { 5, -4,  5,  7 },| have a diagonals    |
+                     * | {77,  5,  0, 74 } |                     |
+                     * | }                 |                     |
+                     * ------------------------------------------
+                     *    
+                     */
+                    //your code here
+
+                }
+                public void StarPrinter(int triangleHight)
+                {
+                    /* Write a programm that will print a triagle of stars  with hight = triangleHight
+                     *  Example: triangleHight = 3;
+                     *  Result:   *
+                     *           * *
+                     *          * * * 
+                     */
+                    //your code here
+                }
+                public void SortList(IList<int> listOfNumbers)
+                {
+                    //Print to console elements of  listOfNumbers in ascending order
+                    //your code here
+                }
+              
+                
+
+            }
 }
--- CLesson_3/Program.cs
+++ CLesson_3/Program.cs
@@ -1,15 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CLesson_3
+namespace Homework
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(String[] args)
         {
+                    Homework homework = new Homework();
+        IList<int> list = new List<int>() { -5, 8, -7, 0, 44, 121, -7 };
+        int[,] matrix1 = new int[3, 3] { { 1, 3, 5 }, { -1, 100, 11 }, { 2, 15, 44 } };
+        int[,] matrix2 = new int[3, 4] { { 1, 6, 21, 8 }, { 5, -4, 5, 7 }, { 77, 5, 0, 74 } };
+
+
+                    homework.GetCentralElementFromMatrix1(matrix1);
+                    homework.GetCentralElementFromMatrix2(matrix2);
+                    homework.GetSummOfDiagonalsElements(matrix1);
+                    homework.GetSummOfDiagonalsElements(matrix2);
+                    homework.StarPrinter(5);
+                    homework.SortList(list);
+            Console.WriteLine();
+            Console.ReadKey();
         }
     }
 }
